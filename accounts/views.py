893b6fc048a8e87c5fb7e1cf546149c14d032ba8
Deeraj Kumar <deerajkmr98@gmail.com>
2021-10-18 12:23:55 +0530

    create user signup form

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . import forms
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class SignUp(CreateView):
+    template_name = 'accounts/signup.html'
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login.html')
