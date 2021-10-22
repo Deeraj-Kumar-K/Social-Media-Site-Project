72555a11677e6f085747b5021b070347b73e5d62
Deeraj Kumar <deerajkmr98@gmail.com>
2021-10-22 16:32:19 +0530

    debug and update homepage view

--- simplesocial/views.py
+++ simplesocial/views.py
@@ -1,10 +1,18 @@
 from django.views.generic import TemplateView
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
-class HomePage(TemplateView):
-    template_name = 'index.html'
 
 class WelcomePage(TemplateView):
     template_name = 'welcome.html'
 
 class ThanksPage(TemplateView):
     template_name = 'thanks.html'
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("welcome"))
+        return super().get(request, *args, **kwargs)
